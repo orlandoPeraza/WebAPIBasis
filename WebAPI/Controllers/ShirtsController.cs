e74ab376d6bb1a5cbf6161c2519e7a0a335fd8a4
Orlando <peraza.oj@gmail.com>
2025-05-28 21:09:34 +0200

    Routing with controllers

---
+++ WebAPI/Controllers/ShirtsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        [Route("/shirts")]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet]
+        [Route("/shirts/{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt: {id}";
+        }
+
+        [HttpPost]
+        [Route("/shirts")]
+        public string CreateShirt()
+        {
+            return "Creating a shirt";
+        }
+
+        [HttpPut]
+        [Route("/shirts/{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating a shirt: {id}";
+        }
+
+        [HttpDelete]
+        [Route("/shirts/{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -8,28 +9,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// Routing
-
-// "/shirts
-app.MapGet("/shirts", () => {
-    return "Reading all the shirts";
-});
-
-app.MapGet("/shirts/{id}", (int id) => {
-    return $"Reading shirt with ID: {id}";
-});
-
-app.MapPost("/shirts", () => {
-    return "Creating a shirt.";
-});
-
-app.MapPut("/shirts/{id}", (int id) => {
-    return $"Updating shirt wirth ID: {id}";
-});
-
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with ID: {id}";
-});
+app.MapControllers();
 
 app.Run();
