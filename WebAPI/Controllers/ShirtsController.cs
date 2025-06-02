e0b2d06d174244ede71b940d23b277bb1483e83a
OrlandoPeraza <peraza.oj@gmail.com>
2025-06-02 19:07:37 +0200

    Returning types

--- WebAPI/Controllers/ShirtsController.cs
+++ WebAPI/Controllers/ShirtsController.cs
@@ -7,16 +7,30 @@ namespace WebAPI.Controllers
     [Route("api/[controller]")]
     public class ShirtsController: ControllerBase
     {
-        [HttpGet]
-        public string GetShirts()
+        private List<Shirt> shirts = new List<Shirt>()
         {
-            return "Reading all the shirts";
+            new Shirt {ShirtId = 1, Brand = "My Brand 1", Color = "red", Gender = "men", Price = 150, Size = 9},
+            new Shirt {ShirtId = 2, Brand = "My Brand 2", Color = "blu", Gender = "women", Price = 150, Size = 5},
+            new Shirt {ShirtId = 3, Brand = "My Brand 3", Color = "green", Gender = "men", Price = 150, Size = 7},
+            new Shirt {ShirtId = 4, Brand = "My Brand 4", Color = "orange", Gender = "women", Price = 150, Size = 6},
+            new Shirt {ShirtId = 5, Brand = "My Brand 5", Color = "yellow", Gender = "men", Price = 150, Size = 11},
+        };
+
+        [HttpGet]
+        public IActionResult GetShirts()
+        {
+            return Ok("Reading all the shirts");
         }
 
         [HttpGet("{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id)
         {
-            return $"Reading shirt: id: {id}";
+            if (id <= 0)
+                return BadRequest();
+            var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+            if (shirt == null)
+                return NotFound();
+            return Ok(shirt);
         }
 
         [HttpPost]
