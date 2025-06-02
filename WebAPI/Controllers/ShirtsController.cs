ea62bd23eaffd97315938b0f77defa8822705013
OrlandoPeraza <peraza.oj@gmail.com>
2025-06-02 16:51:19 +0200

    Using Route Template

--- WebAPI/Controllers/ShirtsController.cs
+++ WebAPI/Controllers/ShirtsController.cs
@@ -3,38 +3,34 @@
 namespace WebAPI.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtsController: ControllerBase
     {
         [HttpGet]
-        [Route("/shirts")]
         public string GetShirts()
         {
             return "Reading all the shirts";
         }
 
-        [HttpGet]
-        [Route("/shirts/{id}")]
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt: {id}";
         }
 
         [HttpPost]
-        [Route("/shirts")]
         public string CreateShirt()
         {
             return "Creating a shirt";
         }
 
-        [HttpPut]
-        [Route("/shirts/{id}")]
+        [HttpPut("{id}")]
         public string UpdateShirt(int id)
         {
             return $"Updating a shirt: {id}";
         }
 
-        [HttpDelete]
-        [Route("/shirts/{id}")]
+        [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting shirt: {id}";
